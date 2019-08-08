5d12be82e36da91ad99274308069767bbc156e3b
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-08-08 10:11:53 +0200

    upd

---
+++ install-operator-sdk.sh
@@ -0,0 +1,15 @@
+#!/bin.sh
+
+# Helper to install operator-sdk 
+
+set -e
+set -x
+
+RELEASE_VERSION=v0.9.0
+curl -OJL https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
+curl -OJL https://github.com/operator-framework/operator-sdk/releases/download/${RELEASE_VERSION}/operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu.asc
+gpg --verify operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu.asc
+chmod +x operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
+sudo mkdir -p /usr/local/bin
+sudo cp operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu /usr/local/bin/operator-sdk
+rm operator-sdk-${RELEASE_VERSION}-x86_64-linux-gnu
