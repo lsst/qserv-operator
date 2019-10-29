f256bd08c58116fa1872bcb30fb1074786c41d3f
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-10-29 05:26:25 -0500

    Add NCSA parameters

--- ncsa_storage/env.sh
+++ ncsa_storage/env.sh
@@ -1,6 +1,6 @@
 QSERV_IMAGE="qserv/qserv:9701693"
 OP_IMAGE="qserv/qserv-operator:v0.0.2"
 
-INSTANCE="qserv"
+INSTANCE="qserv-dev"
 NS="qserv-dev"
 export GO111MODULE=on
---
+++ ncsa_storage/init.sh
@@ -0,0 +1,2 @@
+cp env.sh ..
+cp qserv_v1alpha1_qserv_cr.yaml ../deploy/crds
