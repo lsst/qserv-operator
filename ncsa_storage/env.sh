6ac7e2c8ca19e7800fa5fcbe8f185d8f8e4e2327
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-10-29 05:19:59 -0500

    Add NCSA parameters

--- ncsa_storage/env.sh
+++ ncsa_storage/env.sh
@@ -1,17 +1,6 @@
-# Parameters related to NCSA Qserv cluster
+QSERV_IMAGE="qserv/qserv:9701693"
+OP_IMAGE="qserv/qserv-operator:v0.0.2"
 
-# All worker hosts have same prefix
-HOSTNAME_TPL="lsst-qserv-db"
-
-INSTANCE='qserv-dev'
-
-# First and last id for worker node names
-WORKER_FIRST_ID=01
-WORKER_LAST_ID=30
-
-MASTER="lsst-qserv-master01"
-
-WORKERS=$(seq --format "${HOSTNAME_TPL}%02g" \
-    --separator=" " "$WORKER_FIRST_ID" "$WORKER_LAST_ID")
-
-PARALLEL_SSH_CFG="$HOME/.ssh/sshloginfile"
+INSTANCE="qserv"
+NS="qserv-dev"
+export GO111MODULE=on
