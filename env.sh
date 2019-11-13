67134bafb5492e2d7f4cfed60e8f46acd47d6ed5
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2019-11-13 22:54:50 +0100

    upd

--- env.sh
+++ env.sh
@@ -1,6 +1,4 @@
-QSERV_IMAGE="qserv/qserv:9701693"
 OP_IMAGE="qserv/qserv-operator:v0.0.3"
 
 INSTANCE="qserv"
 NS="default"
-export GO111MODULE=on
--- ncsa_storage/env.sh
+++ ncsa_storage/env.sh
@@ -1,6 +1,4 @@
-QSERV_IMAGE="qserv/qserv:9701693"
-OP_IMAGE="qserv/qserv-operator:v0.0.2"
+OP_IMAGE="qserv/qserv-operator:v0.0.3"
 
 INSTANCE="qserv-dev"
 NS="qserv-dev"
-export GO111MODULE=on
