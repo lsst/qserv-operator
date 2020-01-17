ee7c837ef3bcc1a192ae0145bc275de75f989b6a
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-01-17 10:55:17 +0100

    Improve build workflow

--- push-image.sh
+++ push-image.sh
@@ -9,6 +9,4 @@ set -x
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 . "$DIR/env.sh"
 
-GO111MODULE="on" operator-sdk build "$OP_IMAGE"
-sed "s|REPLACE_IMAGE|$OP_IMAGE|g" "$DIR/deploy/operator.yaml.tpl" \
-    > "$DIR/deploy/operator.yaml"
+docker push "$OP_IMAGE"
