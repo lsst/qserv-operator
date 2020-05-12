44bac60773502cdf2dc2f8381fa6a31c1ee3cd3d
Shivansh Saini <shivanshs9@gmail.com>
2020-05-12 23:14:40 +0530

    Add network policies for worker, repl DB and CZAR

--- pkg/controller/qserv/internal/sync/network.go
+++ pkg/controller/qserv/internal/sync/network.go
@@ -2,15 +2,20 @@ package sync
 
 import (
 	qservv1alpha1 "github.com/lsst/qserv-operator/pkg/apis/qserv/v1alpha1"
+	"github.com/lsst/qserv-operator/pkg/constants"
 	"github.com/lsst/qserv-operator/pkg/scheme/qserv"
 	"github.com/lsst/qserv-operator/pkg/staging/syncer"
+	"github.com/lsst/qserv-operator/pkg/util"
 	"k8s.io/apimachinery/pkg/runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func NewNetworkPoliciesSyncer(r *qservv1alpha1.Qserv, c client.Client, scheme *runtime.Scheme) []syncer.Interface {
-	policy := qserv.GenerateDefaultNetworkPolicy(r, controllerLabels)
+	labels := util.MergeLabels(controllerLabels, util.GetLabels(constants.NetworkPolicy, r.Name))
 	return []syncer.Interface{
-		syncer.NewObjectSyncer("DefaultNetworkPolicy", r, policy, c, scheme, noFunc),
+		syncer.NewObjectSyncer("DefaultNetworkPolicy", r, qserv.GenerateDefaultNetworkPolicy(r, labels), c, scheme, noFunc),
+		syncer.NewObjectSyncer("CzarNetworkPolicy", r, qserv.GenerateCzarNetworkPolicy(r, labels), c, scheme, noFunc),
+		syncer.NewObjectSyncer("ReplDBNetworkPolicy", r, qserv.GenerateReplDBNetworkPolicy(r, labels), c, scheme, noFunc),
+		syncer.NewObjectSyncer("WorkerNetworkPolicy", r, qserv.GenerateWorkerNetworkPolicy(r, labels), c, scheme, noFunc),
 	}
 }
--- pkg/scheme/qserv/network.go
+++ pkg/scheme/qserv/network.go
@@ -6,16 +6,14 @@ import (
 	"github.com/lsst/qserv-operator/pkg/util"
 	v1 "k8s.io/api/networking/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/util/intstr"
 )
 
 func GenerateDefaultNetworkPolicy(cr *qservv1alpha1.Qserv, labels map[string]string) *v1.NetworkPolicy {
-	namespace := cr.Namespace
-	labels = util.MergeLabels(labels, util.GetLabels(constants.NetworkPolicy, cr.Name))
-
 	return &v1.NetworkPolicy{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "default-deny-ingress",
-			Namespace: namespace,
+			Namespace: cr.Namespace,
 			Labels:    labels,
 		},
 		Spec: v1.NetworkPolicySpec{
@@ -23,6 +21,150 @@ func GenerateDefaultNetworkPolicy(cr *qservv1alpha1.Qserv, labels map[string]str
 				v1.PolicyTypeIngress,
 			},
 			PodSelector: metav1.LabelSelector{},
+		},
+	}
+}
+
+func GenerateCzarNetworkPolicy(cr *qservv1alpha1.Qserv, labels map[string]string) *v1.NetworkPolicy {
+	return &v1.NetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "allow-czar-ingress",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: v1.NetworkPolicySpec{
+			PolicyTypes: []v1.PolicyType{
+				v1.PolicyTypeIngress,
+			},
+			PodSelector: metav1.LabelSelector{
+				MatchLabels: util.GetLabels(constants.CzarName, cr.Name),
+			},
+			Ingress: []v1.NetworkPolicyIngressRule{
+				{
+					// DB port
+					Ports: []v1.NetworkPolicyPort{
+						{
+							Port: &intstr.IntOrString{
+								IntVal: 3306,
+							},
+						},
+					},
+					From: []v1.NetworkPolicyPeer{
+						{
+							// Only Replication Controller can access the DB
+							PodSelector: &metav1.LabelSelector{
+								MatchLabels: util.GetLabels(constants.ReplName, cr.Name),
+							},
+						},
+					},
+				},
+				{
+					// Proxy port
+					Ports: []v1.NetworkPolicyPort{
+						{
+							Port: &intstr.IntOrString{
+								IntVal: 4040,
+							},
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func GenerateReplDBNetworkPolicy(cr *qservv1alpha1.Qserv, labels map[string]string) *v1.NetworkPolicy {
+	return &v1.NetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "allow-repl-db-ingress",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: v1.NetworkPolicySpec{
+			PolicyTypes: []v1.PolicyType{
+				v1.PolicyTypeIngress,
+			},
+			PodSelector: metav1.LabelSelector{
+				MatchLabels: util.GetLabels(constants.ReplName, cr.Name),
+			},
+			Ingress: []v1.NetworkPolicyIngressRule{
+				{
+					// DB port
+					Ports: []v1.NetworkPolicyPort{
+						{
+							Port: &intstr.IntOrString{
+								IntVal: 3306,
+							},
+						},
+					},
+					From: []v1.NetworkPolicyPeer{
+						// {
+						// 	// Only Replication Controller can access the DB
+						// 	PodSelector: &metav1.LabelSelector{
+						// 		MatchLabels: util.GetLabels(constants.ReplName, cr.Name),
+						// 	},
+						// },
+					},
+				},
+			},
+		},
+	}
+}
+
+func GenerateWorkerNetworkPolicy(cr *qservv1alpha1.Qserv, labels map[string]string) *v1.NetworkPolicy {
+	return &v1.NetworkPolicy{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "allow-worker-ingress",
+			Namespace: cr.Namespace,
+			Labels:    labels,
+		},
+		Spec: v1.NetworkPolicySpec{
+			PolicyTypes: []v1.PolicyType{
+				v1.PolicyTypeIngress,
+			},
+			PodSelector: metav1.LabelSelector{
+				MatchLabels: util.GetLabels(constants.WorkerName, cr.Name),
+			},
+			Ingress: []v1.NetworkPolicyIngressRule{
+				{
+					// DB port
+					Ports: []v1.NetworkPolicyPort{
+						{
+							Port: &intstr.IntOrString{
+								IntVal: 3306,
+							},
+						},
+					},
+					From: []v1.NetworkPolicyPeer{
+						// {
+						// 	// Only Replication Controller can access the DB
+						// 	PodSelector: &metav1.LabelSelector{
+						// 		MatchLabels: util.GetLabels(constants.ReplName, cr.Name),
+						// 	},
+						// },
+					},
+				},
+				{
+					// Xrootd port
+					Ports: []v1.NetworkPolicyPort{
+						{
+							Port: &intstr.IntOrString{
+								IntVal: 1094,
+							},
+						},
+					},
+				},
+				{
+					// Wmgr port
+					Ports: []v1.NetworkPolicyPort{
+						{
+							Port: &intstr.IntOrString{
+								IntVal: 5012,
+							},
+						},
+					},
+				},
+			},
 		},
 	}
 }
