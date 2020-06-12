a105e0d76d82a79f12e9223be6554752c3d3e8df
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2020-06-12 21:21:36 +0200

    upd

--- configmap/repl-ctl/start/start.sh
+++ configmap/repl-ctl/start/start.sh
@@ -78,7 +78,8 @@ export LSST_LOG_CONFIG="/config-etc/log4cxx.replication.properties"
 CONFIG="mysql://${REPL_DB_USER}:${MYSQL_REPLICA_PASSWORD}@${REPL_DB_DN}:${REPL_DB_PORT}/${REPL_DB}"
 PARAMETERS="--worker-evict-timeout=3600 --health-probe-interval=120 --replication-interval=1200"
 MALLOC_CONF=${OPT_MALLOC_CONF} LD_PRELOAD=${OPT_LD_PRELOAD} \
-qserv-replica-master-http ${PARAMETERS} --config="${CONFIG}" --qserv-db-password="${MYSQL_ROOT_PASSWORD}" --debug
+# qserv-replica-master-http ${PARAMETERS} --config="${CONFIG}" --qserv-db-password="${MYSQL_ROOT_PASSWORD}" --debug
+qserv-replica-master-http ${PARAMETERS} --config="${CONFIG}" --qserv-db-password="${MYSQL_ROOT_PASSWORD}"
 
 # For debug purpose
 #while true;
