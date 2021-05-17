b83f451f4d12c4df458e6688d70bfd36438de715
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-05-17 23:36:48 +0200

    Document how to install release YYYY.M.N

--- examples/aliases-qserv-operator.zsh
+++ examples/aliases-qserv-operator.zsh
@@ -8,9 +8,9 @@ alias cdi="cd $QSERV_INGEST_SRC_DIR"
 
 # Re-install qserv from scratch
 
-alias delete-qserv="kubectl delete qservs.qserv.lsst.org --all && kubectl delete pvc,pv --all"
+alias delete-qserv="kubectl delete qservs.qserv.lsst.org --all && kubectl delete pvc -l app.kubernetes.io/managed-by=qserv-operator"
 alias krq="delete-qserv && kubectl apply -k $QSERV_OPERATOR_SRC_DIR/manifests/base"
-alias gkrq="delete-qserv &&  && kubectl apply -k $QSERV_OPERATOR_SRC_DIR/manifests/gke-qserv-dev"
+alias gkrq="delete-qserv && kubectl apply -k $QSERV_OPERATOR_SRC_DIR/manifests/gke-qserv-dev"
 
 # Rebuild qserv from scratch
 alias rbo="cdo && ./build.sh -k && ./push-image.sh -d && \
