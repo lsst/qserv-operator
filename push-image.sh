8807b1db7f62a0d8a4db9d0e46cdcf4e6e8f9777
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2021-07-15 10:14:16 +0200

    Improve release scripts

--- push-image.sh
+++ push-image.sh
@@ -9,6 +9,8 @@ set -euxo pipefail
 DIR=$(cd "$(dirname "$0")"; pwd -P)
 . "$DIR/env.build.sh"
 
+IMAGE="$OP_IMAGE"
+
 set -e
 
 usage() {
@@ -19,21 +21,21 @@ Usage: `basename $0` [options] path host [host ...]
   Available options:
     -h          this message
     -k          development mode: load image in kind
-    -d          development mode: push image to docker hub
+    -d          do not push image to docker hub
 
-Push image to Docker Hub or load it inside kind
+Push image to Docker Hub and/or load it inside kind
 EOD
 }
 
 kind=false
-dockerhub=false
+dockerhub=true
 
 # get the options
 while getopts dhk c ; do
     case $c in
 	    h) usage ; exit 0 ;;
 	    k) kind=true ;;
-	    d) dockerhub=true ;;
+	    d) dockerhub=false ;;
 	    \?) usage ; exit 2 ;;
     esac
 done
@@ -45,8 +47,8 @@ if [ $# -ne 0 ] ; then
 fi
 
 if [ $kind = true ]; then
-  kind load docker-image "$OP_IMAGE"
+  kind load docker-image "$IMAGE"
 fi
 if [ $dockerhub = true ]; then
-  docker push "$OP_IMAGE"
+  docker push "$IMAGE"
 fi
