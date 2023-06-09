edefc0db453e9dd7599237746f489cd16682943c
Fabrice Jammes <fabrice.jammes@in2p3.fr>
2023-06-09 18:48:39 +0200

    Bump operator-sdk to v1.29.0
    see https://sdk.operatorframework.io/docs/upgrading-sdk-version/v1.29.0/

--- prereq-build.sh
+++ prereq-build.sh
@@ -4,7 +4,7 @@
 
 set -euxo pipefail
 
-RELEASE_VERSION=v1.28.0
+RELEASE_VERSION=v1.29.0
 export ARCH=$(case $(arch) in x86_64) echo -n amd64 ;; aarch64) echo -n arm64 ;; *) echo -n $(arch) ;; esac)
 export OS=$(uname | awk '{print tolower($0)}')
 
